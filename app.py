8947060df80866dedd79d8902aecdcad0604740c
safwanshamsir99 <safwanshamsir7177@gmail.com>
2023-11-28 15:17:11 +0800

    Streamlit development

--- app.py
+++ app.py
@@ -1,4 +1,52 @@
 import streamlit as st
+from PIL import Image
+from app_module import classify
 
+st.set_page_config(page_title='Rice Sure', page_icon='fav.jpeg', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+st.image('ricesurelogo.jpeg', width=200)
 tab1, tab2, tab3 = st.tabs(["Prediction and Quality Check", "Rice Information", "Group Members"])
+
+with tab1:
+        #st.title("Rice Sure")
+        #st.divider()
+        st.subheader("RiceSure app fights fraudulent in the rice industry by using Convolutional Neural Network with Transfer Learning and Fine Tuning to identify rice grain types and ensure consistent quality control.")
+        st.subheader("Some of the rice varieties including Basmati Rice, Jasmine Rice, Karacadag Rice, Arborio Rice, and Ipsala have different features and structure. Let's predict the type of the rice below. ")
+        #st.divider()
+        #st.subheader("Upload rice grain image.")
+        uploaded_file = st.file_uploader("Choose an image of a rice grain. (Kindly attach only rice grain images)")
+        if uploaded_file is not None:
+                image = Image.open(uploaded_file)
+                st.image(image, caption='Uploaded Image')
+                image.save('up.jpg')
+                #st.divider()
+                
+                if st.button('Click to process'):
+                        st.spinner(text="Predicting...")
+                        #st.balloons()
+                        st.success('The image was uploaded successfully and below are the rice type and rice quality check')
+                        label = classify(uploaded_file)
+                        quality = rice_quality_check('up.jpg')
+                        
+                        #res = sign_names.get(label)
+                        st.markdown(label)
+                        st.markdown(quality)
+with tab2:
+        st.subheader("Basmati Rice")
+        st.image("Basmati.jpg", width=200)
+        st.write("Basmati rice is a long-grain rice variety grown primarily in India and Pakistan. It has a distinctive nutty flavor and aroma, and the cooked grains are fluffy and separate, making it a popular choice for biryanis, pilafs, and other Indian and Middle Eastern dishes.")
+        #st.divider()
+        
+        st.subheader("Jasmine Rice")
+        st.image("Jasmine.jpg", width=200)
+        st.write("Jasmine rice is a long-grain rice variety grown in Thailand, Cambodia, and Vietnam. It has a fragrant aroma and a slightly sweet flavor, making it popular in Asian cuisine. Jasmine rice is fluffy and soft when cooked and is often served alongside curries, stir-fries, and other spicy dishes.")
+        #st.divider()
+
+with tab3:
+        st.write("Rajasegaran a/l M Sivaanandan (22052733)")
+        st.write("Basubeit, Omar Gumaan Saleh (22054606)")
+        st.write("Safwan bin Shamsir (S2195293)")
+        st.write("Azle Abd Ghalim (S2036517)")
+        st.write("Intan Nor Qamarina Binti Yunus (S2180929)")
 
--- app_module.py
+++ app_module.py
@@ -1,9 +1,41 @@
-import tensorflow as tf
 from tensorflow.keras.models import load_model
-import os
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import numpy as np
+import tensorflow as tf
+from keras.models import model_from_json
+import cv2
+import matplotlib.pyplot as plt
 
-BEST_MODEL_PATH = os.path.join(os.getcwd(), 'vgg16_tl_ft_solar_dust.h5')
+IMG_SIZE = (224, 224)
 
-best_model = load_model(BEST_MODEL_PATH)
-best_model.summary()
+def load_model():
+    # load json and create model
+    json_file = open('model/best_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model/best_model.h5")
+    return loaded_model
+
+
+def get_image(path, img_size=IMG_SIZE):
+    img = load_img(path, target_size=img_size)
+    img = img_to_array(img) / 255.
+    return img
+
+
+def classify(path):
+    model = load_model()
+    label = ['clean', 'dirty']
+    img = get_image(path, img_size=IMG_SIZE)
+    my_image = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    y_pred = model.predict(my_image)
+    y_hat = np.argmax(y_pr, axis=1)
+    yi_hat = lbl[y_hat[0]]
+    yi_pr = y_pr[0].max()
+    title_sub = f"Prediction: {yi_hat} ({yi_pr:.1%})"
+    print("\n")
+    print(title_sub)
+    return title_sub
 
